36551032c7f9306937ecc3c24f979e757a993b4c
Joni Hyttinen <jonihyt@gmail.com>
2026-05-18 14:29:11 +0300

    Keep track of already used student accounts

--- prepare_users.py
+++ prepare_users.py
@@ -8,7 +8,20 @@ OUTPUT_CSV = Path("files") / "students.csv"
 OUTPUT_MAP = Path("files") / "mapping.txt"
 
 rows = []
+existing_users = {}
 counter = 1
+
+if OUTPUT_CSV.exists():
+    with OUTPUT_CSV.open(newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            existing_users[row["moodle_id"]] = row
+
+def extract_number(username: str) -> int:
+    return int(username.replace("student", ""))
+
+if existing_users:
+    counter = max(extract_number(u["username"]) for u in existing_users.values()) + 1
 
 for folder in sorted([p for p in INPUT_DIR.iterdir() if p.is_dir()]):
     pubkey = None
@@ -18,16 +31,27 @@ for folder in sorted([p for p in INPUT_DIR.iterdir() if p.is_dir()]):
     if not pubkey:
         continue
 
-    username = f"student{counter:04d}"
-    rows.append({"username": username, "moodle_id": folder.name, "pubkey": pubkey})
+    # If user already exists, re-use
+    if folder.name in existing_users:
+        row = existing_users[folder.name]
+        # Update key if changed
+        row["pubkey"] = pubkey
+    else:
+        username = f"student{counter:04d}"
+        row = {
+            "username": username, 
+            "moodle_id": folder.name, 
+            "pubkey": pubkey
+            }
+        existing_users[folder.name] = row
+        counter += 1
 
-    counter += 1
+    rows.append(row)
 
 with OUTPUT_CSV.open("w", newline="") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=["username", "pubkey"])
+    writer = csv.DictWriter(csvfile, fieldnames=["moodle_id", "username", "pubkey"])
     writer.writeheader()
-    for r in rows:
-        writer.writerow({"username": r["username"], "pubkey": r["pubkey"]})
+    writer.writerows(rows)
 
 with OUTPUT_MAP.open("w") as f:
     for r in rows:
